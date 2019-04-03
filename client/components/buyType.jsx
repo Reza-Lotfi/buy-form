5254a97b0927fe6d6206b9d13ed8c7ef41a6b311
Sebastian Ward <sebastianmward419@gmail.com>
2019-04-02 22:05:56 -0700

    Displays buy types, test skeleton for react, more css

---
+++ client/components/buyType.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class BuyType extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dropDownActive : false,
+        }
+    }
+
+    toggleDropDown() {
+        
+        this.setState({
+            dropDownActive : !this.state.dropDownActive
+        })
+        
+    }
+
+    render() {
+        return (
+        <div className="app_buySellTitleButton">
+             <svg  onClick={() => {this.toggleDropDown()}} width="28" height="28" viewBox="0 0 28 28">
+                 <path fillRule="evenodd" d="M14,16 C12.8954305,16 12,15.1045695 12,
+                    14 C12,12.8954305 12.8954305,12 14,12 C15.1045695,12 16,12.8954305 16,
+                    14 C16,15.1045695 15.1045695,16 14,16 Z M6,16 C4.8954305,16 4,15.1045695 4,
+                    14 C4,12.8954305 4.8954305,12 6,12 C7.1045695,12 8,12.8954305 8,14 C8,15.1045695 7.1045695,
+                    16 6,16 Z M22,16 C20.8954305,16 20,15.1045695 20,14 C20,12.8954305 20.8954305,12 22,12 C23.1045695,
+                    12 24,12.8954305 24,14 C24,15.1045695 23.1045695,16 22,16 Z">
+                 </path>
+             </svg>
+             {
+                 this.state.dropDownActive &&
+                 <div id='app_buySellTitleDropdown'>
+                    <div>
+                        <h3 id='app_buySellOrderTypeTitle'>Order Type</h3>
+                    </div>
+                    <p className='buySell_buy-type_dropdown_option'>Market Order</p>
+                    <p className='buySell_buy-type_dropdown_option'>Limit Order</p>
+                    <p className='buySell_buy-type_dropdown_option'>Stop Loss Order</p>
+                    <p className='buySell_buy-type_dropdown_option'>Stop Limit Order</p>
+                </div>
+             }
+        </div>
+        );
+    }
+}
+
+export default BuyType;
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,24 @@
+import { isTSAnyKeyword } from "@babel/types";
+import { shallow } from 'enzyme';
 
 
 test('adds 1 + 2 to equal 3', () => {
     expect(true).toBe(true);
+  });
+
+  describe('functionality of the title section', () => {
+
+    it('Should open up a menu when the button element is pressed', () => {
+
+    });
+
+    it('Pop-up menu should be able to change the state of the App react component', () => {
+
+    });
+
+    it('Depending on price should calculate the price of all the shares you want to buy', () => {
+
+    });
+
   });
 
